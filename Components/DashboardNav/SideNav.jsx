860e557653ad9dc4fb31fcda8b37a4357287d226
Toniodujinrin <todujinrin@gmail.com>
2022-09-16 17:01:48 +0100

    finished all tasks concerning the dashboard main

--- Components/DashboardNav/SideNav.jsx
+++ Components/DashboardNav/SideNav.jsx
@@ -39,7 +39,7 @@ const NavButton = ({ isActive, text, onClick }) => {
 	);
 };
 
-const SideNav = () => {
+const SideNav = ({ open, openSide }) => {
 	const [cookies, setCookie, removeCookie] = useCookies(["user"]);
 	const [currentPage, setCurrentPage] = useState("dashboard");
 	const router = useRouter();
@@ -48,56 +48,124 @@ const SideNav = () => {
 		router.replace("/login");
 	};
 	return (
-		<div
-			style={{ zIndex: 20 }}
-			className="fixed h-screen w-[295px] pl-[32px] pt-[35px] bg-white"
-		>
-			<div className="mb-[40px]">
-				<Image
-					src="/Images/eccLogo.svg"
-					width={82.98}
-					height={28}
-				/>
+		<div>
+			<div
+				style={{ zIndex: 20 }}
+				className="fixed h-screen w-[295px] pl-[32px] pt-[35px] bg-white lg:block hidden"
+			>
+				<div className="mb-[40px]">
+					<Image
+						src="/Images/eccLogo.svg"
+						width={82.98}
+						height={28}
+					/>
+				</div>
+				<div>
+					<button className="min-w-[200px] py-[14px] bg-eccblue font-semibold poppinsFont text-[16px] text-white rounded-xl focus:outline-none mb-[20px]">File a Complaint</button>
+					<NavButton
+						isActive={router.pathname == "/dashboard"}
+						text="Dashboard"
+						onClick={() => router.push("/dashboard")}
+					/>
+					<NavButton
+						isActive={router.pathname == "/dashboard/profile"}
+						text="My Profile"
+						onClick={() => router.push("/dashboard/profile")}
+					/>
+					<NavButton
+						isActive={router.pathname == "/dashboard/mycomplaints" || router.pathname == "/complaintDetails"}
+						text="My Complaints"
+						onClick={() => router.push("/dashboard/mycomplaints")}
+					/>
+					<NavButton
+						isActive={router.pathname == "/dashboard/talktoalawyer"}
+						text="Talk to a Lawyer"
+						onClick={() => setCurrentPage("talktoalawyer")}
+					/>
+					<NavButton
+						isActive={router.pathname == "/dashboard/hirealawyer"}
+						text="Hire a Lawyer"
+						onClick={() => setCurrentPage("hirealawyer")}
+					/>
+					<NavButton
+						isActive={router.pathname == "/dashboard/help"}
+						text="Help"
+						onClick={() => setCurrentPage("help")}
+					/>
+					<button
+						onClick={onLogout}
+						className="transition-[100ms] text-[#FA4343] inline-flex min-w-[198px] py-[15px] bg-white font-medium poppinsFont text-[16px] rounded-xl  px-[16px] items-center gap-x-[16px]"
+					>
+						<img src="/Images/logoutIcon.svg" />
+						Logout
+					</button>
+				</div>
 			</div>
+
 			<div>
-				<button className="min-w-[200px] py-[14px] bg-eccblue font-semibold poppinsFont text-[16px] text-white rounded-xl focus:outline-none mb-[20px]">File a Complaint</button>
-				<NavButton
-					isActive={router.pathname == "/dashboard"}
-					text="Dashboard"
-					onClick={() => router.push("/dashboard")}
-				/>
-				<NavButton
-					isActive={router.pathname == "/dashboard/profile"}
-					text="My Profile"
-					onClick={() => router.push("/dashboard/profile")}
-				/>
-				<NavButton
-					isActive={router.pathname == "/dashboard/mycomplaints" || router.pathname == "/complaintDetails"}
-					text="My Complaints"
-					onClick={() => router.push("/dashboard/mycomplaints")}
-				/>
-				<NavButton
-					isActive={router.pathname == "/dashboard/talktoalawyer"}
-					text="Talk to a Lawyer"
-					onClick={() => setCurrentPage("talktoalawyer")}
-				/>
-				<NavButton
-					isActive={router.pathname == "/dashboard/hirealawyer"}
-					text="Hire a Lawyer"
-					onClick={() => setCurrentPage("hirealawyer")}
-				/>
-				<NavButton
-					isActive={router.pathname == "/dashboard/help"}
-					text="Help"
-					onClick={() => setCurrentPage("help")}
-				/>
-				<button
-					onClick={onLogout}
-					className="transition-[100ms] text-[#FA4343] inline-flex min-w-[198px] py-[15px] bg-white font-medium poppinsFont text-[16px] rounded-xl  px-[16px] items-center gap-x-[16px]"
-				>
-					<img src="/Images/logoutIcon.svg" />
-					Logout
-				</button>
+				{open ? (
+					<div
+						style={{ zIndex: 30 }}
+						className="fixed h-screen w-[295px] pl-[32px] pt-[35px] bg-white "
+					>
+						<div className="mb-[40px] flex flex-row justify-between">
+							<Image
+								src="/Images/eccLogo.svg"
+								width={82.98}
+								height={28}
+							/>
+
+							<button
+								className="text-eccblue mr-4 text-lg "
+								onClick={openSide}
+							>
+								X
+							</button>
+						</div>
+						<div>
+							<button className="min-w-[200px] py-[14px] bg-eccblue font-semibold poppinsFont text-[16px] text-white rounded-xl focus:outline-none mb-[20px]">File a Complaint</button>
+							<NavButton
+								isActive={router.pathname == "/dashboard"}
+								text="Dashboard"
+								onClick={() => router.push("/dashboard")}
+							/>
+							<NavButton
+								isActive={router.pathname == "/dashboard/profile"}
+								text="My Profile"
+								onClick={() => router.push("/dashboard/profile")}
+							/>
+							<NavButton
+								isActive={router.pathname == "/dashboard/mycomplaints" || router.pathname == "/complaintDetails"}
+								text="My Complaints"
+								onClick={() => router.push("/dashboard/mycomplaints")}
+							/>
+							<NavButton
+								isActive={router.pathname == "/dashboard/talktoalawyer"}
+								text="Talk to a Lawyer"
+								onClick={() => setCurrentPage("talktoalawyer")}
+							/>
+							<NavButton
+								isActive={router.pathname == "/dashboard/hirealawyer"}
+								text="Hire a Lawyer"
+								onClick={() => setCurrentPage("hirealawyer")}
+							/>
+							<NavButton
+								isActive={router.pathname == "/dashboard/help"}
+								text="Help"
+								onClick={() => setCurrentPage("help")}
+							/>
+							<button
+								onClick={onLogout}
+								className="transition-[100ms] text-[#FA4343] inline-flex min-w-[198px] py-[15px] bg-white font-medium poppinsFont text-[16px] rounded-xl  px-[16px] items-center gap-x-[16px]"
+							>
+								<img src="/Images/logoutIcon.svg" />
+								Logout
+							</button>
+						</div>
+					</div>
+				) : (
+					<div></div>
+				)}
 			</div>
 		</div>
 	);
