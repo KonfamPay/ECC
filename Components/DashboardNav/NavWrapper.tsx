aa08d02f13cb251d366e2c33685e67ab4fdd2c3c
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-10-04 15:41:33 +0100

    Reduced the top padding on the dashboard

--- Components/DashboardNav/NavWrapper.tsx
+++ Components/DashboardNav/NavWrapper.tsx
@@ -23,7 +23,7 @@ const NavWrapper: React.FC<NavWrapperProps> = ({ children }) => {
 			<TopNav openSide={openSide} />
 			<div className="lg:block hidden">
 				<div className="pl-[295px] pt-[126px] w-full h-screen min-h-screen min-w-screen bg-clearblue">
-					<div className="bg-clearblue w-full min-h-[calc(100vh-126px)] pl-[30px] pr-[40px] pt-[35px] pb-[43px]">{children}</div>
+					<div className="bg-clearblue w-full min-h-[calc(100vh-126px)] pl-[30px] pr-[40px] pt-[0px] pb-[43px]">{children}</div>
 				</div>
 			</div>
 			<div className=" lg:hidden w-full h-screen bg-clearblue">{children}</div>
