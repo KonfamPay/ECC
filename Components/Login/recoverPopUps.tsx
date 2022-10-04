1fac0e21de89eb11be6585a7a672cc0a9639d3b8
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-10-04 15:42:56 +0100

    Extracted two components from the PopUp component

--- Components/Login/recoverPopUps.tsx
+++ Components/Login/recoverPopUps.tsx
@@ -1,8 +1,65 @@
+import { Dispatch, SetStateAction, useEffect } from "react";
+import { motion } from "framer-motion";
+
 interface PopupProps {
 	error: boolean;
 	resend: boolean;
 	exit: any;
 }
+
+interface ErrorPopUpProps {
+	error: string;
+	setBackendError: Dispatch<SetStateAction<string>>;
+}
+
+interface ResendSuccessfulPopUp {
+	resendText: string;
+	setResendText: Dispatch<SetStateAction<string>>;
+}
+
+export const ErrorPopUp: React.FC<ErrorPopUpProps> = ({ error, setBackendError }) => {
+	useEffect(() => {
+		setTimeout(() => setBackendError(""), 1000);
+	}, []);
+	return (
+		<motion.div
+			initial={{ opacity: 0, y: 20 }}
+			animate={{ opacity: 1, y: 0 }}
+			exit={{ opacity: 0 }}
+			layout
+			className=" lg:w-full p-0 lg:h-[72px] rounded-[8px] box-border border border-solid flex flex-col border-[#ef2e2e] justify-center items-center lg:bg-white w-[350px] h-[50px] mx-auto my-8  bg-[#f1f7fe] "
+		>
+			<div className="flex flex-row items-center p-0 gap-5   h-[34]">
+				<img src="/icons/incorrect.svg" />
+				<div className="text-[20px]">{error}</div>
+			</div>
+		</motion.div>
+	);
+};
+
+export const ResendCodeSuccessfulPopUp: React.FC<ResendSuccessfulPopUp> = ({ resendText, setResendText }) => {
+	useEffect(() => {
+		setTimeout(() => setResendText(""), 1000);
+	}, []);
+	return (
+		<motion.div
+			initial={{ opacity: 0, y: 20 }}
+			animate={{ opacity: 1, y: 0 }}
+			exit={{ opacity: 0 }}
+			className=" lg:w-full p-0 lg:h-[72px] rounded-[8px] box-border border border-solid flex flex-col border-[#2f972f] justify-center items-center lg:bg-white w-[350px] h-[50px] mx-auto my-8  bg-[#f1f7fe] "
+		>
+			<div className="flex flex-row items-center p-0 gap-5   h-[34]">
+				<div className="text-[#2f972f]">
+					<img
+						src="./images/icon.png"
+						alt="check"
+					/>
+				</div>
+				<div>{resendText}</div>
+			</div>
+		</motion.div>
+	);
+};
 
 const Popup: React.FC<PopupProps> = ({ error, resend, exit }) => {
 	return (
@@ -10,8 +67,8 @@ const Popup: React.FC<PopupProps> = ({ error, resend, exit }) => {
 			{error && !resend && (
 				<div className=" lg:w-full p-0 lg:h-[72px] rounded-[8px] box-border border border-solid flex flex-col border-[#ef2e2e] justify-center items-center lg:bg-white w-[350px] h-[50px] mx-auto my-8  bg-[#f1f7fe] ">
 					<div className="flex flex-row items-center p-0 gap-5   h-[34]">
-						<div className="text-[#ef2e2e]">X</div>
-						<div>You have entered a wrong code</div>
+						<img src="/icons/incorrect.svg" />
+						<div className="text-[20px]">{error}</div>
 					</div>
 				</div>
 			)}
