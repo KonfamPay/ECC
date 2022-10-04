8ecf4d001608ac2fea9dc7d7aee933e2e22fb05d
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-10-04 15:44:36 +0100

    Added react-query to the project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,19 +5,23 @@ import { CookiesProvider } from "react-cookie";
 import type { AppProps } from "next/app";
 import { AnimatePresence } from "framer-motion";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { QueryClient, QueryClientProvider } from "react-query";
 
+const queryClient = new QueryClient();
 function MyApp({ Component, pageProps, router }: AppProps) {
 	return (
-		<AnimatePresence exitBeforeEnter>
-			<GoogleOAuthProvider clientId="805243689186-pe7uljmhc6i38mpvehe4o768ll6nomd4.apps.googleusercontent.com">
-				<CookiesProvider>
-					<Component
-						{...pageProps}
-						key={router.route}
-					/>
-				</CookiesProvider>
-			</GoogleOAuthProvider>
-		</AnimatePresence>
+		<QueryClientProvider client={queryClient}>
+			<AnimatePresence exitBeforeEnter>
+				<GoogleOAuthProvider clientId="805243689186-pe7uljmhc6i38mpvehe4o768ll6nomd4.apps.googleusercontent.com">
+					<CookiesProvider>
+						<Component
+							{...pageProps}
+							key={router.route}
+						/>
+					</CookiesProvider>
+				</GoogleOAuthProvider>
+			</AnimatePresence>
+		</QueryClientProvider>
 	);
 }
 
