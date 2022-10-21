68acebd35873ff0081f118b2d76908fab623a8ba
toymat <toyymat@gmail.com>
2022-10-21 09:13:55 +0100

    implemented the charts on statistics page

---
+++ pages/statistics/ChartData/ScamData.js
@@ -0,0 +1,32 @@
+const scamStat = [
+	{
+		scamType: "investment",
+		amt: "$4000",
+	},
+	{
+		scamType: "clothing & accessories",
+		amt: "$3000",
+	},
+	{
+		scamType: "travels & tours",
+		amt: "$2000",
+	},
+	{
+		scamType: "Agrucultural businesses",
+		amt: "$1000",
+	},
+	{
+		scamType: "Digital currency scams",
+		amt: "$5000",
+	},
+	{
+		scamType: "False Clergy & others",
+		amt: "$6000",
+	},
+	{
+		scamType: "Some contents",
+		amt: "$7000",
+	},
+];
+
+export default scamStat;
