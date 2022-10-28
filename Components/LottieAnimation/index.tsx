15bbbed10dc92d69bc020e63d3a4c8ec75bf5329
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-10-28 13:09:52 +0100

    Created the LottieAnimation component

---
+++ Components/LottieAnimation/index.tsx
@@ -0,0 +1,28 @@
+import Lottie from "react-lottie";
+
+interface LottieAnimationProps {
+    animationData: any;
+    height: number;
+    width: number;
+}
+const LottieAnimation: React.FC<LottieAnimationProps> = ({ animationData, height, width }) => {
+	const defaultOptions = {
+		loop: true,
+		autoplay: true,
+		animationData: animationData,
+		rendererSettings: {
+			preserveAspectRatio: "xMidYMid slice",
+		},
+	};
+
+	return (
+		<div>
+			<Lottie
+				options={defaultOptions}
+				height={height}
+				width={width}
+			/>
+		</div>
+	);
+};
+export default LottieAnimation;
