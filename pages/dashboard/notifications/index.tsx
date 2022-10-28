3a1cc85ccc741c61a646f687fd37f51fc82294a8
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-10-28 13:18:03 +0100

    Finished working on the notifications page

--- pages/dashboard/notifications/index.tsx
+++ pages/dashboard/notifications/index.tsx
@@ -2,14 +2,18 @@ import type { NextPage } from "next";
 import NavWrapper from "../../../Components/DashboardNav/NavWrapper";
 import NotificationItem from "../../../Components/NotificationItem";
 
-const NotificationsPage: NextPage = ({ notificationData }) => {
+interface NotificationsPageProps {
+	notificationData: { title: string; additionalInfo: string; time: string };
+}
+
+const NotificationsPage: NextPage<NotificationsPageProps> = ({ notificationData }) => {
 	return (
 		<NavWrapper>
-			<div className="w-full rounded-[15px] overflow-hidden">
+			<div className="w-full h-[calc(100vh-170px)] rounded-[15px] overflow-hidden">
 				<div className="py-[16px] pl-[54px] bg-eccblue">
 					<p className="text-[24px] font-medium poppinsFont text-white">All Notifications</p>
 				</div>
-				<div className="pt-[39px] bg-white flex flex-col gap-y-[40px]">
+				<div className="pt-[39px] bg-white flex flex-col gap-y-[40px] overflow-y-scroll h-[calc(100vh-250px)]">
 					{notificationData.map((item: any, index: number) => (
 						<NotificationItem
 							key={index}
