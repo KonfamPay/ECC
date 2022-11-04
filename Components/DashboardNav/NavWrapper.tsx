82a1e663c4ca22d8da0824040b59a9e8733cd405
Itsjustblack <jason.aghedo@gmail.com>
2022-11-04 15:59:29 +0100

    Feat: Made dashboard file-complaint responsive

--- Components/DashboardNav/NavWrapper.tsx
+++ Components/DashboardNav/NavWrapper.tsx
@@ -23,7 +23,7 @@ const NavWrapper: React.FC<NavWrapperProps> = ({ children }) => {
 				<TopNav openSide={openSide} />
 				<div className="lg:block hidden">
 					<div className="pl-[295px] pt-[126px] w-full h-screen min-h-screen min-w-screen bg-clearblue">
-						<div className={`pl-[30px] pr-[40px] pt-[35px] bg-clearblue  w-full min-h-[calc(100vh-126px)]  pb-[43px]`}>{children}</div>
+						<div className={`px-[35px] pt-[35px] bg-clearblue  w-full min-h-[calc(100vh-126px)]  pb-[43px]`}>{children}</div>
 					</div>
 				</div>
 				<div className=" lg:hidden mt-[57.3px] w-full h-screen bg-clearblue">{children}</div>
