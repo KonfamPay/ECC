0611abb88e6b28548eff9d6be49bad8e5ce70db1
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-11-12 22:33:29 +0100

    Refactored the types of a notification and started working on some UI tewals

--- Components/NotificationItem/index.tsx
+++ Components/NotificationItem/index.tsx
@@ -1,17 +1,17 @@
 interface NotificationItemProps {
 	title: string;
-	additionalInfo: string;
+	message: string;
 	time: string;
 	type: string;
 }
-export const NotificationItem: React.FC<NotificationItemProps> = ({ title, additionalInfo, time, type }) => {
+export const NotificationItem: React.FC<NotificationItemProps> = ({ title, message, time, type }) => {
 	const getType = (types: string) => {
-		if (types === "documentation") return "../icons/documentation.svg";
-		if (types === "verification") return "../icons/verification.svg";
-		if (types === "fileRed") return "../icons/fileRed.svg";
-		if (types === "fileGray") return "../icons/fileGray.svg";
-		if (types === "fileOrange") return "../icons/fileOrange.svg";
-		if (types === "fileGreen") return "../icons/fileGreen.svg";
+		if (types === "account") return "../icons/documentation.svg";
+		else if (types === "account") return "../icons/verification.svg";
+		else if (types === "open") return "../icons/fileRed.svg";
+		else if (types === "closed") return "../icons/fileGray.svg";
+		else if (types === "pending") return "../icons/fileOrange.svg";
+		else if (types === "resolved") return "../icons/fileGreen.svg";
 	};
 
 	return (
@@ -26,7 +26,7 @@ export const NotificationItem: React.FC<NotificationItemProps> = ({ title, addit
 					{/* <div className="w-[50px] h-[50px] p-4  bg-gradient-to-b from-eccblue/80 to-eccblue rounded-full "></div> */}
 					<div>
 						<p className="lg:text-[18px] text-[14px] font-medium poppinsFont">{title}</p>
-						<p className="lg:text-[16px] text-[12px] text-grey-200 font-medium poppinsFont">{additionalInfo}</p>
+						<p className="lg:text-[16px] text-[12px] text-grey-200 font-medium poppinsFont">{message}</p>
 					</div>
 				</div>
 				<p className="text-[#666666E8] text-[12px] ml-2">{time}</p>
