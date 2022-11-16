dce919b5b28135c298564ec27267e8a2f20f109a
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-11-16 09:40:30 +0100

    chore: Added the user context to the component tree

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ import { AnimatePresence } from "framer-motion";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 import NotificationContextProvider from "../Components/Contexts/NotificationContext";
+import UserContextProvider from "../Components/Contexts/UserContext";
 
 const queryClient = new QueryClient();
 function MyApp({ Component, pageProps, router }: AppProps) {
@@ -17,10 +18,12 @@ function MyApp({ Component, pageProps, router }: AppProps) {
 				<GoogleOAuthProvider clientId="805243689186-pe7uljmhc6i38mpvehe4o768ll6nomd4.apps.googleusercontent.com">
 					<CookiesProvider>
 						<NotificationContextProvider>
-							<Component
-								{...pageProps}
-								key={router.route}
-							/>
+							<UserContextProvider>
+								<Component
+									{...pageProps}
+									key={router.route}
+								/>
+							</UserContextProvider>
 						</NotificationContextProvider>
 					</CookiesProvider>
 				</GoogleOAuthProvider>
