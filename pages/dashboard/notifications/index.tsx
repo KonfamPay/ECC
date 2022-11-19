1f53e9977f6d55b37d721e376dba2af6ebb397d0
Toniodujinrin <todujinrin@gmail.com>
2022-11-19 15:46:01 +0100

    changes

--- pages/dashboard/notifications/index.tsx
+++ pages/dashboard/notifications/index.tsx
@@ -2,14 +2,7 @@ import type { NextPage } from "next";
 import NavWrapper from "../../../Components/DashboardNav/NavWrapper";
 import NotificationItem from "../../../Components/NotificationItem";
 import { getServerSideProps } from "./../../../Components/NotificationItem/notificationData/index";
-<<<<<<< HEAD
 import { useRouter } from "next/router";
-import { NotificationData } from "./../../../types/complaintTypes.d";
-
-interface NotificationsPageProps {
-	notificationData: NotificationData[];
-=======
-import { useRouter }  from "next/router";
 import { useContext, useEffect } from "react";
 import { NotificationContext } from "../../../Components/Contexts/NotificationContext";
 import { Notification } from "../../../Components/Types";
@@ -19,7 +12,6 @@ import { motion } from "framer-motion";
 
 interface NotificationsPageProps {
 	notificationData: Notification[];
->>>>>>> cb9013a6791cfbfd7b382643bbc3c11487c21328
 }
 
 const NotificationsPage: NextPage<NotificationsPageProps> = () => {
@@ -32,18 +24,17 @@ const NotificationsPage: NextPage<NotificationsPageProps> = () => {
 
 	return (
 		<NavWrapper>
-			<motion.div initial={{opacity: 0, scale: 0.95}} animate={{opacity: 1, scale: 1, transition: {duration: 0.3}}} className="w-full h-full rounded-[15px] overflow-hidden">
+			<motion.div
+				initial={{ opacity: 0, scale: 0.95 }}
+				animate={{ opacity: 1, scale: 1, transition: { duration: 0.3 } }}
+				className="w-full h-full rounded-[15px] overflow-hidden"
+			>
 				<div className="py-[16px] pl-[54px] bg-eccblue">
 					<p className="text-[24px] font-medium poppinsFont text-white">All Notifications</p>
 				</div>
-<<<<<<< HEAD
-				<div className="pt-[39px] bg-white flex flex-col gap-y-[40px] overflow-y-scroll h-full pb-4">
-					{notificationData.map((item, index: number) => (
-=======
+
 				<div className="pt-[39px] pb-[49px] bg-white flex flex-col gap-y-[40px] overflow-y-scroll h-[calc(100vh-280px)] custom-scrollbar">
 					{notificationData?.map((item: any, index: number) => (
-
->>>>>>> cb9013a6791cfbfd7b382643bbc3c11487c21328
 						<div
 							onClick={() => {
 								router.push({ pathname: "/dashboard/notificationDetails", query: { id: item._id } });
