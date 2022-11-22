57882815619d02df39a9ea8f22b9e377249207f6
Itsjustblack <jason.aghedo@gmail.com>
2022-11-22 18:19:28 +0100

    Feat: Made User Dashboard Responsive

--- Components/DashboardComponents/DashboardCard/index.tsx
+++ Components/DashboardComponents/DashboardCard/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { NextPage } from "next";
+import Image from "next/image";
 import { useRouter } from "next/router";
+import leftArrow from "@icons/dashboard-icons/arrow-narrow-left.svg";
 
 const LandingPageCard: NextPage = () => {
 	const router = useRouter();
 	const cardcontents = [
 		{
-			iconpath: "../../../icons/dashboard-icons/my-complaints.svg",
+			iconpath: "/../../../icons/dashboard-icons/my-complaints.svg",
 			title: "My Complaints",
 			description: "Lorem ipsum dolor sit amet, cctetur adipiscing elit ut ",
 			bgcolor: "#0B63C5",
@@ -14,7 +16,7 @@ const LandingPageCard: NextPage = () => {
 			href: "/dashboard/mycomplaints",
 		},
 		{
-			iconpath: "../../../icons/dashboard-icons/help.svg",
+			iconpath: "/../../../icons/dashboard-icons/help.svg",
 			title: "Help",
 			description: "Lorem ipsum dolor sit amet, cctetur adipiscing elit ut ",
 			bgcolor: "#FFB330",
@@ -22,7 +24,7 @@ const LandingPageCard: NextPage = () => {
 			href: "/",
 		},
 		{
-			iconpath: "../../../icons/dashboard-icons/profile.svg",
+			iconpath: "/../../../icons/dashboard-icons/profile.svg",
 			title: "My Portfolio",
 			description: "Lorem ipsum dolor sit amet, cctetur adipiscing elit ut ",
 			bgcolor: "#FF88B3",
@@ -30,7 +32,7 @@ const LandingPageCard: NextPage = () => {
 			href: "/dashboard/profile",
 		},
 		{
-			iconpath: "../../../icons/dashboard-icons/fac.svg",
+			iconpath: "/../../../icons/dashboard-icons/fac.svg",
 			title: "File a Complaint",
 			description: "Lorem ipsum dolor sit amet, cctetur adipiscing elit ut ",
 			bgcolor: "#020D1B",
@@ -39,58 +41,46 @@ const LandingPageCard: NextPage = () => {
 	];
 	return (
 		<>
-			<div className="lg:block hidden">
-				<div className="grid grid-cols-2 gap-x-[30px] xl:gap-x-[68px] gap-y-[20px] xl:gap-y-[37px] ">
+			<div className="block">
+				<div className="grid grid-cols-2 gap-x-[19px] sm:gap-x-[40px] lg:gap-x-[40px] xl:gap-x-[68px] gap-y-[26px] sm:gap-y-[30px] lg:gap-y-[30px] xl:gap-y-[37px] justify-items-center">
 					{cardcontents.map((card, index) => (
-						<div key={index}
-							className="flex flex-row pl-[22px] xl:pl-[32px] py-[32.5px] xl:py-[55.5px] pr-4 text-white rounded-[20px]"
-							style={{ backgroundColor: card.bgcolor }}
-						>
-							<img
-								src={card.iconpath}
-								className=" w-[80px] h-[81px] "
-								alt={card.alt}
-							/>
-
-							<div className="pl-[32px] w-3/4 flex-1 break-words">
-								<p className="text-[24px] font-[600]">{card.title}</p>
-								<p className="w-full text-lg leading-[27px]">{card.description}</p>
-							</div>
-						</div>
-					))}
-				</div>
-			</div>
-			<div className="lg:hidden block">
-				<div className="grid grid-cols-2 gap-x-[19px] gap-y-[26px] justify-items-center">
-					{cardcontents.map((card) => (
 						<div
-							className="flex flex-col sm:pl-[10px] sm:pt-4  w-[250px] pl-[15px] py-[10px] text-white rounded-[20px]"
+							key={index}
+							className="relative flex flex-col lg:cursor-pointer px-[10px] lg:pr-[20px] sm:pl-[20px] xl:pl-[32px] pt-[17px] pb-[11px] sm:py-[40px] xl:py-[55.5px] w-full text-white rounded-[10px] justify-center"
+							onClick={() => {
+								router.push(`${card.href}`);
+							}}
 							style={{ backgroundColor: card.bgcolor }}
 						>
-							<img
-								src={card.iconpath}
-								className="w-[48px] h-[48px]"
-								alt={card.alt}
-							/>
-
-							<div className="pl-[10px] w-full mt-4">
-								<p className="text-[14px] font-[600]">{card.title}</p>
-								<p className="w-full mt-1 text-[13px]">{card.description}</p>
+							<div className="flex flex-col sm:flex-row sm:items-center">
+								<div className="relative w-[32px] sm:w-[60px] xl:w-[80px] h-[32px] sm:h-[60px] xl:h-[80px]">
+									<Image
+										src={card.iconpath}
+										alt={card.alt}
+										layout="fill"
+										objectFit="contain"
+									/>
+								</div>
+								<div className="sm:pl-5 xl:pl-8 w-full lg:w-3/4 flex-1 mt-3 sm:mt-0 break-words mb-2">
+									<p className="text-[14px] sm:text-[20px] lg:text-[24px	] font-[600]">{card.title}</p>
+									<p className="w-full mt-2 sm:mt-0 text-[12px] sm:text-[15px] lg:text-[18px] leading-[18px] sm:leading-[27px]">{card.description}</p>
+								</div>
 							</div>
 
-							<div className="w-full flex flex-col items-end">
-								<button
-									className="mr-4"
+							<div className="flex lg:hidden justify-end relative sm:absolute sm:bottom-[10px] lg:bottom-4 sm:right-4 lg:right-4">
+								<div
+									className="relative w-[20px] lg:w-[25px] h-[20px] lg:h-[25px]"
 									onClick={() => {
 										router.push(`${card.href}`);
 									}}
 								>
-									<img
-										className="w-[20px] h-[20px] text-right "
-										src="../../../icons/dashboard-icons/arrow-narrow-left.svg"
-										alt=""
+									<Image
+										src={leftArrow}
+										alt="a left arrow icon"
+										layout="fill"
+										objectFit="contain"
 									/>
-								</button>
+								</div>
 							</div>
 						</div>
 					))}
--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,4 +1,6 @@
 import type { NextPage } from "next";
+import Image from "next/image";
+import manFreakedOut from "@images/man-freaked-out-2.png";
 import { useState, useEffect, useContext } from "react";
 import DashboardCard from "../../Components/DashboardComponents/DashboardCard";
 import NavWrapper from "../../Components/DashboardNav/NavWrapper";
@@ -27,55 +29,63 @@ const index: NextPage = () => {
 		} else {
 			setUser(cookie.user);
 			console.log(cookie.user);
-			setInterval(() => fetchNotificationData(), 30000)
-			
+			setInterval(() => fetchNotificationData(), 30000);
 		}
 	}, []);
 
 	return (
 		<NavWrapper>
-			<motion.div initial={{opacity: 0, scale: 0.95}} animate={{opacity: 1, scale: 1, transition: {duration: 0.3}}} className="h-full">
-				<div className="pt-0">
+			<motion.div
+				initial={{ opacity: 0, scale: 0.95 }}
+				animate={{ opacity: 1, scale: 1, transition: { duration: 0.3 } }}
+				className="h-full"
+			>
+				<div className="pb-[50px]">
 					<div className="hidden lg:block">
-						<div className="bg-[#020D1B] w-full h-[255px] mt-10 rounded-[20px] flex flex-row relative">
-							<div className="text-white pl-[43px] py-[58.5px]">
-								<p className="text-[40px] leading-[60px] font-[600]">Hi, {user.firstName}</p>
-								<p className="text-[24px] leading-[36px] font-[600] w-[503px]">
+						<div className="bg-[#020D1B] w-full max-h-[255px] mt-10 rounded-[20px] flex flex-row justify-between items-center relative">
+							<div className="text-white pl-[23px] xl:pl-[43px] w-full flex flex-col justify-center">
+								<p className="text-[30px] xl:text-[40px] font-[600]">Hi, {user.firstName}</p>
+								<p className="mt-2 xl:mt-[13px] text-[20px] xl:text-[24px] leading-[25px] xl:leading-[36px]">
 									Vendor don do you strong thing again? <br /> We are ready to listen to your complaints
 								</p>
 							</div>
-							<div className="w-[50%]">
-								<img
-									src="./images/man-freaked-out.png"
-									className="absolute right-[21.28px] -top-[30.54px] h-[285.54px] max-w-full w-[377.72px] "
-									alt=""
+							<div className="h-[285.54px] w-[377.72px] relative bottom-[14px] right-[10px] xl:right-[21px] shrink-0">
+								<Image
+									src={manFreakedOut}
+									layout="fill"
+									objectFit="contain"
+									alt="a man freaked out"
 								/>
 							</div>
 						</div>
-						<div className="mt-[59px] mx-auto  ">
+						<div className="mt-[59px] mx-auto">
 							<DashboardCard />
 						</div>
 					</div>
-					<div className="lg:hidden mt-6">
-						<div className="flex flex-col items-center">
-							<div className="bg-black w-[90%] mt-12 rounded-[10px]  h-fit flex flex-row justify-between items-center">
-								<div className="flex flex-col w-[50%] py-4 text-white pl-4">
-									<p className="text-[16px]">Hi, {user.firstName}</p>
-									<p className="text-[12px]">
-										Vendor don do you strong thing again?
-										<br />
-										We are ready to listen to your complaints
-									</p>
+					<div className="lg:hidden block">
+						<div className="flex flex-row justify-center">
+							<div className="flex flex-col items-center w-full">
+								<div className="bg-[#020D1B] mt-12 rounded-[10px] h-fit flex flex-row justify-between items-center w-full">
+									<div className="flex flex-col py-[14px] sm:py-[25px] text-white pl-3 sm:pl-[20px]">
+										<p className="text-[16px] sm:text-[20px] font-semibold">Hi, {user.firstName}</p>
+										<p className="mt-[4px] text-[12px] sm:text-[16px] leading-[15px] sm:leading-[20px]">
+											Vendor don do you strong thing again?
+											<br />
+											We are ready to listen to your complaints
+										</p>
+									</div>
+									<div className="relative w-[149px] h-[110px] top-0 min-[320px]:top-[3px] min-[392px]:top-0 sm:top-[7px] shrink-0">
+										<Image
+											src={manFreakedOut}
+											alt="A man freaked out"
+											layout="fill"
+											objectFit="contain"
+										/>
+									</div>
 								</div>
-
-								<img
-									className=" w-[149px] h-[95px] sm:w-[190px] sm:h-[140px]  sm:mb-3"
-									src="./images/man-freaked-out.svg"
-									alt=""
-								/>
 							</div>
 						</div>
-						<div className="mt-[59px] px-[12px]">
+						<div className="mt-[35px] mx-auto">
 							<DashboardCard />
 						</div>
 					</div>
