01f46cbb9e02d7d47c3c6bca31457afcab28195e
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-12-06 14:53:46 +0100

    Fixed aos error

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import AOS from "aos";
+// import AOS from "aos";
+import Script from "next/script"
 
 export default function Document() {
 	return (
@@ -8,7 +9,7 @@ export default function Document() {
 			<body>
 				<Main />
 				<NextScript />
-				<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+				<script src="https://unpkg.com/aos@next/dist/aos.js" async></script>
 				<script
 					src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js"
 					defer
