a92ef02c980142e8e26917ce523ed5df4741fd6d
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-12-07 21:28:08 +0100

    Changed casing error in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	important: true,
-	content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/**/*.{js,ts,jsx,tsx}", "./sections/**/**/*.{js,ts,jsx,tsx}"],
+	content: ["./pages/**/*.{js,ts,jsx,tsx}", "./Components/**/**/*.{js,ts,jsx,tsx}", "./sections/**/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {
 			colors: {
@@ -29,7 +29,5 @@ module.exports = {
 	variants: {
 		extend: {},
 	},
-	plugins: [
-		require('tw-elements/dist/plugin')
-	],
+	plugins: [require("tw-elements/dist/plugin")],
 };
