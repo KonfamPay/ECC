81480f9f8c854156f4b41e02a263007ebbfe1f9f
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-12-08 14:40:57 +0100

    added home page to package.lock.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	important: true,
-	content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}", "./sections/**/*.{js,ts,jsx,tsx}"],
+	content: ["./pages/**/*.{js,ts,jsx,tsx}", "./pages/index.tsx", "./components/**/*.{js,ts,jsx,tsx}", "./sections/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {
 			colors: {
