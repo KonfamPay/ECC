641bdbc5ced765a6ddaf08fb75266b647d98d330
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-12-08 14:37:09 +0100

    fix: prettier errors

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,21 +37,21 @@ const Home: NextPage = () => {
 			</Head>
 			<NavBar searchIconIsPresent={true} />
 			<div className="pt-48 mx-auto items-center justify-center text-center bg-clearblue">
-			<p className="mx-auto text-[30px] sm:text-4xl font-bold">
-				Make & <span className="text-eccblue">resolve</span>
-				<br />
-				complaints easily
-			</p>
-			<p className="mt-4 font-medium sm:text-[14px] px-[15px] sm:px-[0]">
-				Hair vendor don block you <br />
-				after you make payment for Instagram?
-			</p>
-			<img
-				src="./images/man-freaked-out.png"
-				alt=""
-				className="mx-auto"
-			/>
-		</div>
+				<p className="mx-auto text-[30px] sm:text-4xl font-bold">
+					Make & <span className="text-eccblue">resolve</span>
+					<br />
+					complaints easily
+				</p>
+				<p className="mt-4 font-medium sm:text-[14px] px-[15px] sm:px-[0]">
+					Hair vendor don block you <br />
+					after you make payment for Instagram?
+				</p>
+				<img
+					src="./images/man-freaked-out.png"
+					alt=""
+					className="mx-auto"
+				/>
+			</div>
 			<OurGoalsSection />
 			<Numbers />
 			<VentComplaintsSection />
